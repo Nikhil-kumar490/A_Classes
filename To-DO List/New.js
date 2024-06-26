e3fb4f17759848ca6effdd4f368ff85a68981b2f
Nikhil-kumar490 <927nikhilkumar@gmail.com>
2024-06-26 20:06:41 +0530

    javascript

---
+++ To-DO List/New.js
@@ -0,0 +1,7 @@
+document.getElementById('addTaskBtn').addEventListener('click', addTask);
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+
+}
