0e0c209986bb3372c5eea655144054d6b8208434
Nikhil-kumar490 <927nikhilkumar@gmail.com>
2024-06-27 19:39:36 +0530

    jsm

--- To-DO List/New.js
+++ To-DO List/New.js
@@ -1,7 +1,20 @@
-document.getElementById('addTaskBtn').addEventListener('click', addTask);
+function addOne(){
+    var task= document.getElementById("taskInput").value;
 
-function addTask() {
-    const taskInput = document.getElementById('taskInput');
-    const taskText = taskInput.value.trim();
+    document.getElementById("todoItems").innerHTML += "<div  class='con'><div>"+task+"</div><div onclick='rem(this);'>X</div></div>";
 
+    document.getElementById("taskInput").value="";
+}
+
+function rem(elem){
+    var task2= elem.parentElement.innerText.split("\n")[0];
+
+    document.getElementById("completedList").innerHTML +="<div  class='con'><div>"+task2+"</div><div onclick='com(this);'>X</div></div>";
+
+    elem.parentElement.remove();
+
+}
+
+function com(el){
+    el.parentElement.remove();
 }
